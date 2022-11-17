f516bdb3b0a380afc52e6495f260a469bddbd880
KaioValls <vallsbusiness.acc@gmail.com>
2022-11-16 22:14:57 -0300

    first commit

---
+++ js/func.js
@@ -0,0 +1,9 @@
+function atomicBlack() {
+    window.location = "https://ev.braip.com/ref?pv=pron5n2e&af=afinng8p3";
+}
+function hialuroniCaps() {
+    window.location = "https://ev.braip.com/ref?pv=prolzj2o&af=afizlg5pm";
+}
+function liftDetox() {
+    window.location = "https://ev.braip.com/pv/liponwvg/afinng8np";
+}
